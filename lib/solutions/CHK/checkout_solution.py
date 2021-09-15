67673135bbad620695a6e8f4602fa0a929cda3df
sbx_user1051 <sbx_user1051@169.254.16.109>
2021-09-15 11:08:28 +0000

    Wed Sep 15 11:08:28 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -20,6 +20,13 @@ discount_table = {
     }
 }
 
+
+def calculate_discounted_price(item, quantity):
+    non_discounted_quantity = quantity % discount_table[item]
+    discounted_quantity = math.floor(quantity % discount_table[item])
+    return non_discounted_quantity * price_table[item] + discounted_quantity * discount_table[item]
+
+
 def checkout(skus):
     if len(skus) == 0:
         return -1
@@ -40,13 +47,12 @@ def checkout(skus):
             return -1
         
         if item in discount_table:
-            non_discounted_quantity = quantity % discount_table[item]
-            discounted_quantity = math.floor(quantity % discount_table[item])
-            total_checkout_value += non_discounted_quantity * price_table[item] + discounted_quantity * discount_table[item]
+            total_checkout_value += calculate_discounted_price(item, quantity)
         else:
             total_checkout_value += quantity * price_table[item]
 
     return total_checkout_value
+
 
 
 

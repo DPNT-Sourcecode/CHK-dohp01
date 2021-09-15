c1cef3069f3559d7a4aed519bf1b24e63f97436a
sbx_user1051 <sbx_user1051@169.254.16.109>
2021-09-15 10:41:28 +0000

    Wed Sep 15 10:41:28 UTC 2021

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -5,5 +5,17 @@ class TestChk():
     def test_illegal(self):
         assert checkout_solution.checkout("E") == -1
 
-    def test_(self):
-        assert checkout_solution.checkout("C") == -1
+    def test_single(self):
+        assert checkout_solution.checkout("C") == 20
+
+    def test_multi(self):
+        assert checkout_solution.checkout("CD") == 20 + 15
+
+    def test_special_offer(self):
+        assert checkout_solution.checkout("AAA") == 130
+
+    def test_multi_special_offer(self):
+        assert checkout_solution.checkout("AAABB") == 130 + 45
+
+    def test_multi_special_offer_(self):
+        assert checkout_solution.checkout("AAABB") == 130 + 45        
